c79567009faa8cbbd77e8ae29de91c5493d2381f
walton <zjxcdut@gmail.com>
2024-03-26 13:20:19 -0300

    Added Model

---
+++ Groceries/Models/Grocery.cs
@@ -0,0 +1,12 @@
+﻿namespace Groceries.Models
+{
+    public class Grocery
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public string ImageFileName { get; set; } = string.Empty;
+        public decimal Price { get; set; }
+        public DateTime PublishDate { get; set; }
+    }
+}
