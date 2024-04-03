d88a97f91706edb8b91e66bda5637374e1c5dd3b
Junxiao <W0486229@campus.nscc.ca>
2024-04-02 22:51:31 -0300

    Finished product list in loading page.

--- Groceries/Pages/Index.cshtml.cs
+++ Groceries/Pages/Index.cshtml.cs
@@ -1,20 +1,28 @@
+using Groceries.Data;
+using Groceries.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
 
 namespace Groceries.Pages
 {
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+        private readonly GroceriesContext _context;
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public IList<Grocery> Groceries { get; set; } = default!;
+
+        public IndexModel(ILogger<IndexModel> logger, GroceriesContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
-        public void OnGet()
-        {
 
+        public async Task OnGetAsync()
+        {
+            Groceries = await _context.Grocery.ToListAsync();
         }
     }
 }
